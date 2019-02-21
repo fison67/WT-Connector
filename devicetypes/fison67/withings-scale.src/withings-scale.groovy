56453a2a41ac00a45a54163a11b8a6d9e856f230
chals <fison67@nate.com>
2019-02-21 14:02:09 +0900

    Update withings-scale.groovy

--- devicetypes/fison67/withings-scale.src/withings-scale.groovy
+++ devicetypes/fison67/withings-scale.src/withings-scale.groovy
@@ -37,18 +37,16 @@ metadata {
         capability "Temperature Measurement"		
         
         attribute "status", "number"
-        attribute "height", "number"
         attribute "weight", "number"
-        attribute "bmi", "number"
         attribute "fat_free_mass", "number"
         attribute "fat_ratio", "number"
         attribute "fat_mass_weight", "number"
+        attribute "heart_rate", "number"
+        
         attribute "body_temperature", "number"
         attribute "skin_temperature", "number"
         attribute "blood_pressure_max", "number"
         attribute "blood_pressure_min", "number"
-        attribute "heart_rate", "number"
-        
         
         attribute "lastCheckin", "Date"
 	}
@@ -80,20 +78,6 @@ metadata {
             state("val", label:'${currentValue}', defaultState: true
             )
         }
-        valueTile("height_label", "", decoration: "flat", width: 3, height: 1) {
-            state "default", label:'Height'
-        }   
-        valueTile("height", "device.height", width: 3, height: 1, unit: "") {
-            state("val", label:'${currentValue}', defaultState: true
-            )
-        }
-        valueTile("bmi_label", "", decoration: "flat", width: 3, height: 1) {
-            state "default", label:'BMI'
-        }   
-        valueTile("bmi", "device.bmi", width: 3, height: 1, unit: "") {
-            state("val", label:'${currentValue}', defaultState: true
-            )
-        }
         valueTile("fat_free_mass_label", "", decoration: "flat", width: 3, height: 1) {
             state "default", label:'Fat Free Mass'
         }    
@@ -112,34 +96,6 @@ metadata {
             state "default", label:'Fat Mass Weight'
         }    
         valueTile("fat_mass_weight", "device.fat_mass_weight", width: 3, height: 1, unit: "") {
-            state("val", label:'${currentValue}', defaultState: true
-            )
-        }
-        valueTile("body_temperature_label", "", decoration: "flat", width: 3, height: 1) {
-            state "default", label:'Body Temperature'
-        }    
-        valueTile("body_temperature", "device.body_temperature", width: 3, height: 1, unit: "") {
-            state("val", label:'${currentValue}', defaultState: true
-            )
-        }
-        valueTile("skin_temperature_label", "", decoration: "flat", width: 3, height: 1) {
-            state "default", label:'Skin Temperature'
-        }    
-        valueTile("skin_temperature", "device.skin_temperature", width: 3, height: 1, unit: "") {
-            state("val", label:'${currentValue}', defaultState: true
-            )
-        }
-        valueTile("blood_pressure_max_label", "", decoration: "flat", width: 3, height: 1) {
-            state "default", label:'Blood Pressure Max'
-        }    
-        valueTile("blood_pressure_max", "device.blood_pressure_max", width: 3, height: 1, unit: "") {
-            state("val", label:'${currentValue}', defaultState: true
-            )
-        }
-        valueTile("blood_pressure_min_label", "", decoration: "flat", width: 3, height: 1) {
-            state "default", label:'Blood Pressure Min'
-        }    
-        valueTile("blood_pressure_min", "device.blood_pressure_min", width: 3, height: 1, unit: "") {
             state("val", label:'${currentValue}', defaultState: true
             )
         }
@@ -179,8 +135,6 @@ def _getData(){
     def params = [
     	uri: "https://wbsapi.withings.net/measure?action=getmeas&access_token=${accessToken}&category=1&startdate=${first}&enddate=${end}"
     ]
-    log.debug "URL >> ${params}" 
- //   log.debug "ID: ${state._device_id}"
     httpGet(params) { resp ->
         def result =  new JsonSlurper().parseText(resp.data.text)
         if(result.status == 0){
@@ -191,61 +145,77 @@ def _getData(){
             list.each { item ->
             	def subList = item.measures
                 subList.each { subItem ->
-    			//	log.debug "Data DevID: ${item.deviceid}"
                 	if(item.deviceid == state._device_id){
+                        def unitVal = 1
+                    	/*
+                        switch(subItem.unit){
+                        case 0:
+                        	break
+                        case -1:
+                        	unitVal = 10
+                        	break
+                        case -2:
+                        	unitVal = 100
+                        	break
+                        case -3:
+                        	unitVal = 1000
+                        	break
+                        }*/
+                        def _tmp = subItem.unit
+                        while(_tmp < 0){
+                        	_tmp++
+                            unitVal = unitVal * 10
+                        }
+                        
                     	if(subItem.type == 1 && type1Check == false){
-                            sendEvent(name: "status", value: (subItem.value / 1000))
-                            sendEvent(name: "weight", value: (subItem.value / 1000))
-                            log.debug "Weight >> " + (subItem.value / 1000)
+                            sendEvent(name: "status", value: (subItem.value / unitVal))
+                            sendEvent(name: "weight", value: (subItem.value / unitVal))
+                            log.debug "Weight >> " + (subItem.value / unitVal)
                             type1Check = true
                         }else if(subItem.type == 4 && type4Check == false){
-                            sendEvent(name: "height", value: subItem.value / 10)
-                            log.debug "Height >> " + (subItem.value / 10)
+                            sendEvent(name: "height", value: subItem.value / unitVal)
+                            log.debug "Height >> " + (subItem.value / unitVal)
                             type4Check = true
                         }else if(subItem.type == 5 && type5Check == false){
-                            sendEvent(name: "fat_free_mass", value: (subItem.value / 1000))
-                            log.debug "Fat Free Mass >> " + (subItem.value / 1000)
+                            sendEvent(name: "fat_free_mass", value: (subItem.value / unitVal))
+                            log.debug "Fat Free Mass >> " + (subItem.value / unitVal)
                             type5Check = true
                         }else if(subItem.type == 6 && type6Check == false){
-                            sendEvent(name: "fat_ratio", value: (subItem.value / 1000))
-                            log.debug "Fat Ratio >> " + (subItem.value / 1000)
+                            sendEvent(name: "fat_ratio", value: (subItem.value / unitVal))
+                            log.debug "Fat Ratio >> " + (subItem.value / unitVal)
                             type6Check = true
                         }else if(subItem.type == 8 && type8Check == false){
-                            sendEvent(name: "fat_mass_weight", value: (subItem.value / 1000))
-                            log.debug "Fat Mass Weight >> " + (subItem.value / 1000)
+                            sendEvent(name: "fat_mass_weight", value: (subItem.value / unitVal))
+                            log.debug "Fat Mass Weight >> " + (subItem.value / unitVal)
                             type8Check = true
                         }else if(subItem.type == 9 && type9Check == false){
                             sendEvent(name: "blood_pressure_max", value: (subItem.value / 1000))
                             log.debug "Blood Pressure Max >> " + (subItem.value / 1000)
                             type9Check = true
                         }else if(subItem.type == 10 && type10Check == false){
-                            sendEvent(name: "blood_pressure_min", value: (subItem.value / 1000))
-                            log.debug "Blood Pressure Min >> " + (subItem.value / 1000)
+                            sendEvent(name: "blood_pressure_min", value: (subItem.value / unitVal))
+                            log.debug "Blood Pressure Min >> " + (subItem.value / unitVal)
                             type10Check = true
                         }else if(subItem.type == 11 && type11Check == false){
-                            sendEvent(name: "heart_rate", value: (subItem.value ))
-                            log.debug "Heart Rate >> " + (subItem.value)
+                            sendEvent(name: "heart_rate", value: (subItem.value / unitVal ))
+                            log.debug "Heart Rate >> " + (subItem.value / unitVal)
                             type11Check = true
                         }else if(subItem.type == 12 && type12Check == false){
-                            sendEvent(name: "temperature", value: (subItem.value / 1000))
-                            log.debug "Temperature >> " + (subItem.value / 1000)
+                            sendEvent(name: "temperature", value: (subItem.value / unitVal))
+                            log.debug "Temperature >> " + (subItem.value / unitVal)
                             type12Check = true
                         }else if(subItem.type == 71 && type71Check == false){
-                            sendEvent(name: "body_temperature", value: (subItem.value / 1000))
-                            log.debug "Body Temperature >> " + (subItem.value / 1000)
+                            sendEvent(name: "body_temperature", value: (subItem.value / unitVal))
+                            log.debug "Body Temperature >> " + (subItem.value / unitVal)
                             type71Check = true
                         }else if(subItem.type == 73 && type73Check == false){
-                            sendEvent(name: "skin_temperature", value: (subItem.value / 1000))
-                            log.debug "Skin Temperature >> " + (subItem.value / 1000)
+                            sendEvent(name: "skin_temperature", value: (subItem.value / unitVal))
+                            log.debug "Skin Temperature >> " + (subItem.value / unitVal)
                             type73Check = true
                         }
                     }
-                    
             	}
             }
-            
-            sendEvent(name: "bmi", value: (device.currentValue("weight") / device.currentValue("height") * 2))
-            
         }else{
         	log.debug result
             parent.getAccessTokenByRefreshToken("user.metrics")
@@ -254,180 +224,6 @@ def _getData(){
         sendEvent(name: "lastCheckin", value: time, displayed: false)
     }
     
-}
-
-def getTypeName(type){
-	def name = ""
-	switch(type){
-    case 1:
-    	name = "Weight"
-    	break
-    case 4:
-    	name = "Height"
-    	break
-    case 5:
-    	name = "Lean Mass"
-    	break
-    case 6:
-    	name = "Fat Mass"
-    	break
-    case 7:
-    	name = "Lean Mass"
-    	break
-    case 8:
-    	name = "Fat Mass"
-    	break
-    case 9:
-    	name = "Diastolic Blood Pressure"
-    	break
-    case 10:
-    	name = "Systolic Blood Pressure"
-    	break
-    case 11:
-    	name = "Heart Rate"
-    	break
-    case 12:
-    	name = "Temperature"
-    	break
-    case 13:
-    	name = "Humidity"
-    	break
-    case 15:
-    	name = "Noise"
-    	break
-    case 18:
-    	name = "Weight Objective Speed"
-    	break
-    case 19:
-    	name = "Breastfeeding"
-    	break
-    case 20:
-    	name = "Bottle"
-    	break
-    case 22:
-    	name = "BMI"
-    	break
-    case 35:
-    	name = "CO2"
-    	break
-    case 36:
-    	name = "Steps"
-    	break
-    case 37:
-    	name = "Elevation"
-    	break
-    case 38:
-    	name = "Calories"
-    	break
-    case 39:
-    	name = "Intensity"
-    	break
-    case 40:
-    	name = "Distance"
-    	break
-    case 41:
-    	name = "Descent"
-    	break
-    case 42:
-    	name = "Activity Type"
-    	break
-    case 43:
-    	name = "Duration"
-    	break
-    case 44:
-    	name = "Sleep State"
-    	break
-    case 46:
-    	name = "User Event"
-    	break
-    case 47:
-    	name = "Meal Calories"
-    	break
-    case 48:
-    	name = "Active Calories"
-    	break
-    case 49:
-    	name = "Idle Calories"
-    	break
-    case 50:
-    	name = "Inactive Duration"
-    	break
-    case 51:
-    	name = "Light Activity"
-    	break
-    case 52:
-    	name = "Moderate Activity"
-    	break
-    case 53:
-    	name = "Intense Activity"
-    	break
-    case 54:
-    	name = "SpO2"
-    	break
-    case 56:
-    	name = "Ambient light"
-    	break
-    case 57:
-    	name = "Respiratory rate"
-    	break
-    case 58:
-    	name = "Air Quality"
-    	break
-    case 60:
-    	name = "PIM movement"
-    	break
-    case 61:
-    	name = "Maximum movement"
-    	break
-    case 66:
-    	name = "Pressure"
-    	break
-    case 71:
-    	name = "Body Temperature"
-    	break
-    case 72:
-    	name = "GPS Speed"
-    	break
-    case 73:
-    	name = "Skin Temperature"
-    	break
-    case 76:
-    	name = "Muscle Mass"
-    	break
-    case 77:
-    	name = "Water Mass"
-    	break
-    case 87:
-    	name = "Active Calories"
-    	break
-    case 88:
-    	name = "Bone Mass"
-    	break
-    case 91:
-    	name = "Pulse Wave Velocity"
-    	break
-    case 93:
-    	name = "Muscle Mass"
-    	break
-    case 94:
-    	name = "Bone Mass"
-    	break
-    case 95:
-    	name = "Hydration"
-    	break
-    case 96:
-    	name = "Horizontal Radius"
-    	break
-    case 97:
-    	name = "Altitude"
-    	break
-    case 98:
-    	name = "Latitude"
-    	break
-    case 99:
-    	name = "Longitude"
-    	break
-    }
 }
 
 def updated() {
